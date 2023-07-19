3aa8827089bcec28673e98ba0041248a8b909733
alakina.svetlana <alakina.svetlana@ALAKINA-NB-W10>
2023-07-19 16:01:52 +0400

    Add project files.

---
+++ BlazorApp1/Shared/MyComponent .cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp1.Shared
+{
+    /// <summary>
+    /// See <see cref="MainLayout">Test</see> end of Test.
+    /// </summary>
+    public partial class MyComponent
+    {
+        string test = "test";
+    }
+}
